fb067fa8b89b9b99b8fe1698f4745c0f5c904ce4
unknown <luthandodidiza197@gmail.com>
2023-01-09 07:40:57 +0200

    added slider

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+$('.slider').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  prevArrow:'<i class="fas fa-angle-left"></i>',
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+})
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -25,3 +62,6 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += "active";
 }
+
+
+
